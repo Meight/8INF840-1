69f10e602161c46f9b959515dac7d96f5629a43c
Khuuj <ninotna95@hotmail.fr>
2018-06-15 13:35:35 -0400

    correction & fonction isEqual; separation fonction pour images en niveaux de gris et en couleur; optimisation en retirant img des arguments de encode)

--- TP2/QuadTree/Frame.cpp
+++ TP2/QuadTree/Frame.cpp
@@ -1,100 +1,141 @@
 #include "Frame.h"
-#include <thread>
-#include <future>
-
 bool Frame::isEqual(Frame *frame)
 {
-	if (frame->isGrey())
-	{
-		if (this->m_grey == frame->m_grey) { return true; }
-		else return false;
-	}
-	else
-	{
-		if ((this->m_red == frame->m_red) && (this->m_blue == frame->m_blue) && (this->m_green == frame->m_green)) { return true; }
-		else return false; 
-	}
+	if (this->m_grey == frame->m_grey) { return true; }
+	else return false;
+}
+
+bool Frame::isEqualColor(Frame *frame)
+{
+	if ((this->m_red == frame->m_red) && (this->m_blue == frame->m_blue) && (this->m_green == frame->m_green)) { return true; }
+	else return false;
 
 }
 
 
 static bool isMonochrome(QuadTree<Frame> * nw, QuadTree<Frame> * ne, QuadTree<Frame> * se, QuadTree<Frame> * sw) {
-	return (sw->isLeaf() && nw->isLeaf() & ne->isLeaf() && se->isLeaf() && (sw->value().isEqual(&se->value())) && (sw->value().isEqual(&ne->value())) && (sw->value().isEqual(&nw->value())));
+	return (sw->isLeaf() && nw->isLeaf() && ne->isLeaf() && se->isLeaf() && (sw->value().isEqual(&se->value())) && (sw->value().isEqual(&ne->value())) && (sw->value().isEqual(&nw->value())));
 }
 
+static bool isMonochromeColor(QuadTree<Frame> * nw, QuadTree<Frame> * ne, QuadTree<Frame> * se, QuadTree<Frame> * sw) {
+	return (sw->isLeaf() && nw->isLeaf() && ne->isLeaf() && se->isLeaf() && (sw->value().isEqualColor(&se->value())) && (sw->value().isEqualColor(&ne->value())) && (sw->value().isEqualColor(&nw->value())));
+}
 
-QuadTree<Frame>* encodeFrame(int size, int i, int j, CImg<float> image)
+CImg<int> imagecon("soleil.png");
+
+QuadTree<Frame>* encodeFrame(int size, int i, int j)
 {
-	std::cout << "encode\n";
+	//std::cout << "encode\n";
 
 	if (size == 1)
-	{ 
-		QuadLeaf<Frame> *leaf;
-		if (image.spectrum() == 3)
-			leaf = new QuadLeaf<Frame>(Frame(image(i, j, 0), image(i, j, 1), image(i, j, 2), size));
-		else
-			leaf = new QuadLeaf<Frame>(Frame(image(i, j), size));
-		return leaf;
-	}
+		return new QuadLeaf<Frame>(Frame(imagecon(i, j), size));
 
-	int m = size % 2;
 	int s = size / 2;
 
-	/*auto futuresw = std::thread(encodeFrame, s, i, j + s, image);
-	auto futurenw = std::thread(encodeFrame, s, i, j , image);
-	auto futurene = std::thread(encodeFrame, s, i+s, j , image);
-	auto futurese = std::thread(encodeFrame, s, i+s, j + s, image);
+	int js = j + s;
+	int is = i + s;
 
-
-	QuadTree<Frame> *sw = futuresw.get();
-	QuadTree<Frame> *nw = futurenw.get();
-	QuadTree<Frame> *ne = futurene.get();
-	QuadTree<Frame> *se = futurese.get();*/
-
-	QuadTree<Frame> *sw = encodeFrame(s, i, j + s, image);
-	QuadTree<Frame> *nw = encodeFrame(s, i, j, image);
-	QuadTree<Frame> *ne = encodeFrame(s, i+s, j , image);
-	QuadTree<Frame> *se = encodeFrame(s, i + s, j + s, image);
+	QuadTree<Frame> *sw = encodeFrame(s, i, js);
+	QuadTree<Frame> *nw = encodeFrame(s, i, j);
+	QuadTree<Frame> *ne = encodeFrame(s, is, j);
+	QuadTree<Frame> *se = encodeFrame(s, is, js);
 
 	if (isMonochrome(nw, ne, se, sw))
 	{
 		Frame frameleaf = sw->value();
-		frameleaf.setSize(frameleaf.getSize() + se->value().getSize());
+		frameleaf.setSize(size);
 		return new QuadLeaf<Frame>(frameleaf);
+		//return new QuadLeaf<Frame>(sw->value().setSize(sw->value().getSize()));
 	}
 
 	return new QuadNode<Frame>(nw, ne, se, sw);
 }
 
-CImg<float> decodeFrame(QuadTree<Frame> *quadFrame)
+
+QuadTree<Frame>* encodeFrameColor(int size, int i, int j)
 {
-	std::cout << "decode\n";
+	//std::cout << "encode\n";
+
+	if (size == 1)
+	{
+		return new QuadLeaf<Frame>(Frame(imagecon(i, j, 0), imagecon(i, j, 1), imagecon(i, j, 2), size));
+	}
+
+	int s = size / 2;
+	int is = i + s;
+	int js = j + s;
+
+	QuadTree<Frame> *sw = encodeFrameColor(s, i, js);
+	QuadTree<Frame> *nw = encodeFrameColor(s, i, j);
+	QuadTree<Frame> *ne = encodeFrameColor(s, is, j);
+	QuadTree<Frame> *se = encodeFrameColor(s, is, js);
+
+	if (isMonochromeColor(nw, ne, se, sw))
+	{
+		Frame frameleaf = sw->value();
+		frameleaf.setSize(size);
+		return new QuadLeaf<Frame>(frameleaf);
+		//return new QuadLeaf<Frame>(sw->value().setSize(s));
+	}
+
+	return new QuadNode<Frame>(nw, ne, se, sw);
+}
+
+CImg<int> decodeFrame(QuadTree<Frame> *quadFrame)
+{
+	//std::cout << "decode\n";
 
 	if (quadFrame != NULL)
 	{
 		if (quadFrame->isLeaf())
 		{
 			Frame monochromframe = quadFrame->value();
-			if (monochromframe.isGrey())
-			{
-				CImg<float> leafimg(1, 1, 1, 1, monochromframe.getGrey());
-				leafimg.resize(monochromframe.getSize(), monochromframe.getSize());
-				return leafimg;
-			}
-			else
-			{
-				CImg<float> leafimg(1, 1, 1, 3, monochromframe.getRed(), monochromframe.getGreen(), monochromframe.getBlue());
-				leafimg.resize(monochromframe.getSize(), monochromframe.getSize());
-				return leafimg;
-			}
+			//utiliser fill a la place de resize?
+			CImg<int> leafimg(1, 1, 1, 1, monochromframe.getGrey());
+			leafimg.resize(monochromframe.getSize(), monochromframe.getSize());
+			return leafimg;
 		}
 
 		else
 		{
-			CImg<float> nw = decodeFrame(quadFrame->son(0));
-			CImg<float> ne = decodeFrame(quadFrame->son(1));
-			CImg<float> se = decodeFrame(quadFrame->son(2));
-			CImg<float> sw = decodeFrame(quadFrame->son(3));
+			CImg<int> nw = decodeFrame(quadFrame->son(0));
+			CImg<int> ne = decodeFrame(quadFrame->son(1));
+			CImg<int> se = decodeFrame(quadFrame->son(2));
+			CImg<int> sw = decodeFrame(quadFrame->son(3));
+
+			nw.append(sw, 'y');
+			ne.append(se, 'y');
+			nw.append(ne, 'x');
+
+			return nw;
+		}
+	}
+}
+
+CImg<int> decodeFrameColor(QuadTree<Frame> *quadFrame)
+{
+	//std::cout << "decode\n";
+
+	if (quadFrame != NULL)
+	{
+		if (quadFrame->isLeaf())
+		{
+			Frame monochromframe = quadFrame->value();
+			int size = monochromframe.getSize();
+			CImg<int> leafimg(1, 1, 1, 3, monochromframe.getRed(), monochromframe.getGreen(), monochromframe.getBlue());
+			for (int k = 0; k < log2(size); k++)
+			{
+				leafimg.resize_doubleXY();
+			}
+			return leafimg;
+		}
+
+		else
+		{
+			CImg<int> nw = decodeFrameColor(quadFrame->son(0));
+			CImg<int> ne = decodeFrameColor(quadFrame->son(1));
+			CImg<int> se = decodeFrameColor(quadFrame->son(2));
+			CImg<int> sw = decodeFrameColor(quadFrame->son(3));
 
 			nw.append(sw, 'y');
 			ne.append(se, 'y');
@@ -106,10 +147,11 @@ CImg<float> decodeFrame(QuadTree<Frame> *quadFrame)
 }
 
 
+
 int main()
 {
-	//CImg<float> image(INPUT_FILENAME);
-	CImg<float> image("soleil.png");
+	//CImg<int> image(INPUT_FILENAME);
+	/*CImg<int> image("tableau.png");
 
 	int height = image.height();
 	int width = image.width();
@@ -117,20 +159,44 @@ int main()
 	if (width != height)
 		exit(1);
 
-	QuadTree<Frame> *quadimage;
-	quadimage = encodeFrame(width, 0, 0, image);
+
+	if (log2(height) != floor(log2(height)))
+		exit(2);*/
+
+	//ajouter demande de fichier
+	//ajouter demande de nom de fichier de sauvegarde
+	//gerer images non puissance de 2
+
+	int width = imagecon.width();
+
+	if (imagecon.spectrum() == 1)
+	{
+		QuadTree<Frame> *quadimage;
+		quadimage = encodeFrame(width, 0, 0);
+
+		CImg<int> newimage = decodeFrame(quadimage);
+		newimage.save("newtableau.png");
+	}
+
+	else
+	{
+		QuadTree<Frame> *quadimage;
+		std::cout << "starting encode\n";
+		quadimage = encodeFrameColor(width, 0, 0);
 
 
-	float x = 5;
-	
-	CImg<float> newimage = decodeFrame(quadimage);
+		std::cout << "starting decode\n";
+		CImg<int> newimage = decodeFrameColor(quadimage);
+		newimage.save("newsoleil.png");
+	}
 
-	newimage.save("newsoleil.png");
 
-	/*CImg<float> subimage1;
+	int x = 4;
+
+	/*CImg<int> subimage1;
 	subimage1 = image.get_crop(0, 0, width / 2 -1, height - 1);
 
-	CImg<float> subimage2;
+	CImg<int> subimage2;
 	subimage2 = image.get_crop(width / 2, 0, width-1,  height - 1);
 	subimage1.append(subimage2, 'x');
 	subimage1.save_png("chatreconstruit.png");*/
--- TP2/QuadTree/Frame.h
+++ TP2/QuadTree/Frame.h
@@ -8,54 +8,55 @@ class Frame
 
 public:
 
-	Frame(float red, float green, float blue, int m_size /*int width, int height*/)
+	Frame(int red, int green, int blue, int m_size /*int width, int height*/)
 	{
 		m_red = red;
 		m_green = green;
 		m_blue = blue;
 
-		bool is_grey = false;
+		//bool is_grey = false;
 
 		/*m_width = width;
 		m_height = height;*/
 
 	}
 
-	Frame(float grey, int size)
+	Frame(int grey, int size)
 	{
 		m_grey = grey;
 		m_size = size;
-		is_grey = true;
+		//is_grey = true;
 	}
 	
-	float getRed() { return m_red; }
-	float getGreen() { return m_green; }
-	float getBlue() { return m_blue; }
+	int getRed() { return m_red; }
+	int getGreen() { return m_green; }
+	int getBlue() { return m_blue; }
 
-	float getGrey() { return m_grey; }
-	bool isGrey() { return is_grey; }
+	int getGrey() { return m_grey; }
+	//bool isGrey() { return is_grey; }
 
 	int getSize() { return m_size; }
-	int getWidth() { return m_width; }
-	int getheight() { return m_height; }
+	//int getWidth() { return m_width; }
+	//int getheight() { return m_height; }
 
 	void setSize(int size) { m_size = size; }
 
-	bool isEqual(Frame *Frame);
+	inline bool isEqual(Frame *Frame);
+	inline bool isEqualColor(Frame *frame);
 
 
 private:
 
-	float m_red;
-	float m_blue;
-	float m_green;
+	int m_red;
+	int m_blue;
+	int m_green;
 
-	float m_grey;
-	bool is_grey;
+	int m_grey;
+	//bool is_grey;
 
 	int m_size;
-	int m_width;
-	int m_height;
+	//int m_width;
+	//int m_height;
 
 
 };
