5e8c082c382151c4f1c4b46f3fe5675bf3aedc26
florian <florian.dasilva@ensiie.fr>
2018-06-20 23:17:22 -0400

    modification affichage des kd-trees

--- TP2/KdTree/Main.cpp
+++ TP2/KdTree/Main.cpp
@@ -2,8 +2,8 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include "KdTree.h"
+#include <iostream>
 
 int main() {
 	KdTree<2,int>* t = new KdTree<2,int>();
@@ -22,17 +22,31 @@ int main() {
 	t->insert(e);
 	t->insert(f);
 
-	int g[2] = { 85,90 };
-	int h[2] = { 66,86 };
-
-	std::cout << (t->search(g) ? "true\n" : "false\n");
-	std::cout << (t->search(h) ? "true\n" : "false\n");
-
 	t->printTree();
 
-	int* x = t->nearestNeighbor(h);
+	std::cout << "\n";
 
-	std::cout << "(" << x[0] << "," << x[1] << ")\n";
+	int g[2] = { 85,90 };
+	int h[2] = { 70,80 };
+
+	std::cout << "recherche de (" << g[0] << "," << g[1] << "): " << (t->search(g) ? "true\n" : "false\n");
+	std::cout << "recherche de (" << h[0] << "," << h[1] << "): " << (t->search(h) ? "true\n" : "false\n");
+
+	std::cout << "\n";
+
+	int i[2] = { 10,10 };
+	
+	int* x = t->nearestNeighbor(h);
+	std::cout << "plus proche voisin de (" << h[0] << "," << h[1] << "): (" << x[0] << "," << x[1] << ")\n";
+	delete[] x;
+	x = t->nearestNeighbor(i);
+	std::cout << "plus proche voisin de (" << i[0] << "," << i[1] << "): (" << x[0] << "," << x[1] << ")\n";
+
+	std::cout << "\n";
+
+	t->remove(a);
+	std::cout << "suppression de (" << a[0] << "," << a[1] << ")\nnouvel arbre:\n";
+	t->printTree();
 
 	system("PAUSE");
 	return 0;
