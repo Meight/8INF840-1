ad6b3175430293471e83d72cfea88de8f22be8d4
florian <florian.dasilva@ensiie.fr>
2018-06-20 22:54:58 -0400

    fin exercice 1

--- TP2/KdTree/Main.cpp
+++ TP2/KdTree/Main.cpp
@@ -8,14 +8,12 @@
 int main() {
 	KdTree<2,int>* t = new KdTree<2,int>();
 
-
 	int a[2] = { 40,45 };
 	int b[2] = { 70,10 };
 	int c[2] = { 69,50 };
 	int d[2] = { 66,85 };
 	int e[2] = { 85,90 };
 	int f[2] = { 15,70 };
-	int i[2] = { 8,10 };
 
 	t->insert(a);
 	t->insert(b);
@@ -23,18 +21,18 @@ int main() {
 	t->insert(d);
 	t->insert(e);
 	t->insert(f);
-	t->insert(i);
 
 	int g[2] = { 85,90 };
-	int h[2] = { 12,42 };
+	int h[2] = { 66,86 };
 
 	std::cout << (t->search(g) ? "true\n" : "false\n");
 	std::cout << (t->search(h) ? "true\n" : "false\n");
 
+	t->printTree();
 
-	t->remove(b);
+	int* x = t->nearestNeighbor(h);
 
-	std::cout << *t;
+	std::cout << "(" << x[0] << "," << x[1] << ")\n";
 
 	system("PAUSE");
 	return 0;
